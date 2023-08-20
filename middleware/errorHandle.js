0d6c9ff46d6f72e63bc2e310fcc123dc64af4033
Mubosher Muydinov <muboshermuydinov5@gmail.com>
2023-08-20 20:12:55 +0500

    Error write log config added

---
+++ middleware/errorHandle.js
@@ -0,0 +1,23 @@
+import winston from "winston";
+const logger = new winston.Logger({
+  level: "error",
+  transports: [new winston.transports.File({ filename: "error.log" })],
+});
+
+export const errorHandle = (arg) => {
+  if (!arg?.err) return;
+  console.log(arg.err);
+  const formatTime = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const errorMessage = `${formatTime.format(new Date())} ${arg?.err}`;
+
+  logger.log("error", errorMessage);
+  if (arg?.res)
+    return arg?.res?.status(500).json({ message: "Something went wrong" });
+};
